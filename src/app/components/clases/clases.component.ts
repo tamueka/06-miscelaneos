8f5ea1fb92d6f8c8d8bfaee010712eab2e8b5369
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-03-06 12:35:30 +0100

    creamos componente clases inicializamos variables y objetos para utlizar en la vista + probamos clases y estilos en la vista + añadimos app-clases al app.componet.html

---
+++ src/app/components/clases/clases.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html',
+  styles: []
+})
+export class ClasesComponent implements OnInit {
+
+  alerta: string = 'alert-danger';
+  loading: boolean = false;
+
+  propiedades: any = {
+    danger: true
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  ejecutar(){
+
+    this.loading = true;
+
+    setTimeout(() => this.loading = false, 3000 )
+
+  }
+
+
+
+}
